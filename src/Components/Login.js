c410efe6924bbf1443044832f04b08da7c981f46
Rohit Guati <rawheat.gulati@hotmail.com>
2021-04-10 10:56:48 +0530

    Wire login form to Auth api

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -4,11 +4,13 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 export function Login() {
 
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
 
     const { isUserLoggedIn, loginUserWithCredentials } = useAuth();
     
     const loginHandler = () => {
-        loginUserWithCredentials("rohit", "honeySingh");
+        loginUserWithCredentials(username, password);
     }
 
     return(
@@ -19,8 +21,8 @@ export function Login() {
             <div class="form">
                 <div class="form-wrapper">
                     <h1>NFT Baazar</h1>
-                    <input placeholder="Enter your username..." />
-                    <input type="password" placeholder="Password..." />
+                    <input placeholder="Enter your username..." onChange={(event) => setUsername(event.target.value)} />
+                    <input type="password" placeholder="Password..." onChange={(event) => setPassword(event.target.value)} />
                     <button class="btn btn-primary" onClick={loginHandler} >Log In</button>
                     <p>Don't have an accont? <Link to="/signup"><span>Sign Up</span></Link></p>
                 </div>
